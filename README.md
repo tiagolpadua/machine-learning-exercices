129354bf176a4c58d9db018ba115ceafa049a09a
tiagolpadua <tiagolpadua@gmail.com>
2017-11-23 18:34:47 -0200

    ex5

---
+++ README.md
@@ -0,0 +1,7 @@
+OSX
+cd /usr/local/bin
+sudo ln -s /Applications/Octave.app/Contents/Resources/usr/bin/octave-cli
+
+octave-cli
+pwd
+cd to directory
--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -19,16 +19,12 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
-
-
-
-
-
-
-
-
-
-
+h = X*theta;
+J = sum (((h-y).^2))/(2*m)+ lambda * (sum(theta.^2)-theta(1,1)^2)/(2*m);
+grad = X'*(h-y)/m;
+thetaT = theta;
+thetaT(1,1)=0;
+grad+= (lambda*thetaT)/m ;
 
 % =========================================================================
 
